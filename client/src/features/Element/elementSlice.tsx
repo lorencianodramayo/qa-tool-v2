53512770b8379f474335d086effbf2fdaf6d70e3
Denny Marc Maquiling <denny-marc.maquiling@smartly.io>
2023-07-28 00:15:38 +0800

    Empty commit

--- client/src/features/Element/elementSlice.tsx
+++ client/src/features/Element/elementSlice.tsx
@@ -1,55 +1,93 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import elementService from "./elementService";
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit'
+import elementService from './elementService'
 const initialState = {
   languages: null,
   isLanguagesError: false,
   isLanguagesSuccess: false,
   isLanguagesLoading: false,
   languagesMessage: null,
-};
+  uploadXlsx: null,
+  isUploadXlsxError: false,
+  isUploadXlsxSuccess: false,
+  isUploadXlsxLoading: false,
+  uploadXlsxMessage: null,
+}
 export const getLanguages = createAsyncThunk(
-  "element/getLanguages",
+  'element/getLanguages',
   async (payload: any, thunkAPI) => {
     try {
-      return await elementService.getLanguages(payload);
+      return await elementService.getLanguages(payload)
     } catch (error) {
-      const message = error;
-      return thunkAPI.rejectWithValue(message);
+      const message = error
+      return thunkAPI.rejectWithValue(message)
     }
   },
-);
+)
+export const postUploadXlsx = createAsyncThunk(
+  'element/postUploadXlsx',
+  async (payload: any, thunkAPI) => {
+    try {
+      return await elementService.postUploadXlsx(payload)
+    } catch (error) {
+      const message = error
+      return thunkAPI.rejectWithValue(message)
+    }
+  },
+)
 export const Element = createSlice({
-  name: "element",
+  name: 'element',
   initialState,
   reducers: {
     reset: (state, action: PayloadAction<any>) => {
-        state.languages = null;
-        state.isLanguagesError = false;
-        state.isLanguagesSuccess = false;
-        state.isLanguagesLoading = false;
-        state.languagesMessage = "";
+      state.languages = null
+      state.isLanguagesError = false
+      state.isLanguagesSuccess = false
+      state.isLanguagesLoading = false
+      state.languagesMessage = null
+      state.uploadXlsx = null
+      state.isUploadXlsxError = false
+      state.isUploadXlsxSuccess = false
+      state.isUploadXlsxLoading = false
+      state.uploadXlsxMessage = null
     },
   },
   extraReducers: (builder) => {
     builder.addCase(getLanguages.pending, (state) => {
-        state.isLanguagesLoading = true;
-    });
+      state.isLanguagesLoading = true
+    })
     builder.addCase(getLanguages.fulfilled, (state, action) => {
-        state.isLanguagesLoading = false;
-        state.isLanguagesError = false;
-        state.isLanguagesSuccess = true;
-        state.languagesMessage = "";
-        state.languages = action.payload;
-    });
+      state.isLanguagesLoading = false
+      state.isLanguagesError = false
+      state.isLanguagesSuccess = true
+      state.languagesMessage = ''
+      state.languages = action.payload
+    })
     builder.addCase(getLanguages.rejected, (state, action) => {
-        state.isLanguagesLoading = false;
-        state.isLanguagesError = true;
-        state.isLanguagesSuccess = false;
-        state.languagesMessage = action.payload;
-        state.languages = null;
-    });
+      state.isLanguagesLoading = false
+      state.isLanguagesError = true
+      state.isLanguagesSuccess = false
+      state.languagesMessage = action.payload
+      state.languages = null
+    })
+    builder.addCase(postUploadXlsx.pending, (state) => {
+      state.isUploadXlsxLoading = true
+    })
+    builder.addCase(postUploadXlsx.fulfilled, (state, action) => {
+      state.isUploadXlsxLoading = false
+      state.isUploadXlsxError = false
+      state.isUploadXlsxSuccess = true
+      state.uploadXlsxMessage = ''
+      state.uploadXlsx = action.payload
+    })
+    builder.addCase(postUploadXlsx.rejected, (state, action) => {
+      state.isUploadXlsxLoading = false
+      state.isUploadXlsxError = true
+      state.isUploadXlsxSuccess = false
+      state.uploadXlsxMessage = action.payload
+      state.uploadXlsx = null
+    })
   },
-});
+})
 
-export default Element.reducer;
-export const { reset } = Element.actions;
+export default Element.reducer
+export const {reset} = Element.actions
---
+++ server/api/elementTemplate.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const Multer = require("multer");
+const xlsx = require("xlsx");
+const router = express.Router();
+require("dotenv").config();
+const { Language } = require("../models/language");
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 10000 * 1024 * 1024,
+  },
+});
+router.post("/element/upload/xlsx", multer.single("file"), async (req, res) => {
+  try {
+    const fileContent = req.file.buffer;
+    const workbook = xlsx.read(fileContent);
+    const worksheet = workbook.Sheets["Language"];
+    const urlRegex = new RegExp("^https?://", "i");
+    const languages = [];
+    for (const cellAddress in worksheet) {
+      if (
+        cellAddress.startsWith("A") &&
+        worksheet.hasOwnProperty(cellAddress)
+      ) {
+        const value = worksheet[cellAddress].v;
+        const columnBAddress = "B" + cellAddress.substring(1);
+        if (worksheet.hasOwnProperty(columnBAddress)) {
+          const valueInColumnB = worksheet[columnBAddress].v;
+          if (
+            valueInColumnB !== null &&
+            valueInColumnB !== undefined &&
+            valueInColumnB !== "" &&
+            !urlRegex.test(valueInColumnB)
+          ) {
+            languages.push({ language: value, content: valueInColumnB });
+          }
+        }
+      }
+    }
+    Language.deleteMany({}, (err) => {
+      if (err) {
+        res.status(500).json(error);
+      }
+    });
+    Language.create(languages, (err, savedLanguages) => {
+      if (err) res.status(500).json({ data: null, message: err });
+      else res.status(200).json({ data: savedLanguages, message: null });
+    });
+  } catch (error) {
+    res.status(500).json({ data: null, message: error });
+  }
+});
+module.exports = router;
